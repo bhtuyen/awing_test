00a129a44e2c83c3bb5ff3b38370bb0553696f29
Bùi Huy Tuyền <me.bhtuyen@gmail.com>
2026-01-16 19:56:57 +0700

    git commit -m "chore: initial project setup" \   -m "Setup React + TypeScript + Vite project with Material UI and Tailwind CSS"

---
+++ frontend/src/App.tsx
@@ -0,0 +1,10 @@
+import Button from '@mui/material/Button';
+function App() {
+  return (
+    <Button variant='contained' className='bg-amber-400!'>
+      Hello world
+    </Button>
+  );
+}
+
+export default App;
